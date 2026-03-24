90d3a720c12e0db5d087f8a88ca7af0aa2380c29
Stephane Kabangu <stkabang@mi-r4-p3.s19.be>
2026-03-24 19:54:01 +0100

    ex01 loading

---
+++ ex01/Span.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stkabang <stkabang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/24 19:48:19 by stkabang          #+#    #+#             */
+/*   Updated: 2026/03/24 19:53:41 by stkabang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+
+class Span {
+	private :
+		Span();
+	public :
+		Span(int N);
+		Span(const Span &other);
+		Span& operator=(const Span &other);
+		~Span();
+		void addNumber();
+		int shortestSpan();
+		int longestSpan();
+}
+
+#endif
